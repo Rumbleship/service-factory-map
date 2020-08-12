374f0eb7955f8868bb477284c77db9171c1cccfd
Isaac Sparling <223606+isparling@users.noreply.github.com>
2020-08-12 15:08:02 -0700

    :arrow_up_down: add testClear to reset map

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,6 +4,7 @@ export interface RFIFactory<T> {
 export declare abstract class ServiceFactoryMap extends Map<string, RFIFactory<any>> {
 }
 declare class SingletonMap extends ServiceFactoryMap {
+    testClear(force?: boolean): void;
 }
 export declare const ServiceFactories: SingletonMap;
 export {};
